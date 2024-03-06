4df7c45913cb5d33967ac49cee0ec1e7cf6b37ea
rakanmalhaqbani@gmail.com <rakanmalhaqbani@gmail.com>
2024-03-06 12:33:54 +0000

    commit

---
+++ models/staging/raw/stg_raw__criteo.sql
@@ -0,0 +1,24 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'criteo') }}
+
+),
+
+renamed as (
+
+    select
+        date_date,
+        paid_source,
+        campaign_key,
+        campaing_name,
+        Cast(ads_cost as FLOAT64),
+        impression,
+        click
+
+    from source
+
+)
+
+select * from renamed
